d7f47f4259491e7ac45335a314ca60c4a31453fc
coderlyu <974257574@qq.com>
2020-06-30 18:33:05 +0800

    解决 require.context 报错

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,7 +2,9 @@ import Vue from "vue";
 import SvgIcon from "@/components/SvgIcon/index.vue";
 
 Vue.component("svg-icon", SvgIcon);
-
+// interface require {
+//   [prop: string]: any;
+// }
 const req = require.context("./svg", false, /\.svg$/);
 const requireAll = (requireContext: any) =>
   requireContext.keys().map(requireContext);
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,6 @@ import getters from "./getters";
 
 Vue.use(Vuex);
 
-
 const modulesFiles = require.context("./modules", true, /\.js$/);
 // const modulesFiles = require.context("./modules", true, /\.js$/);
 
